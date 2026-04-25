37a6c1d2e7bec4a2b57ac664755a395994515350
deji <ayodeji2k@gmail.com>
2026-04-25 22:35:18 +0100

    refactor BrevoSendEmailRequest to remove unused replyTo field and streamline attachment declaration

--- src/main/java/xy/mailsenders/mail/brevo/BrevoSendEmailRequest.java
+++ src/main/java/xy/mailsenders/mail/brevo/BrevoSendEmailRequest.java
@@ -10,10 +10,9 @@ public class BrevoSendEmailRequest {
 
     private BrevoEmailContact sender;
     private List<BrevoEmailContact> to;
-    private BrevoEmailContact replyTo;
     private String subject;
     private String htmlContent;
     private String textContent;
     private Map<String, String> headers;
-    private java.util.List<BrevoAttachment> attachment;
+    private List<BrevoAttachment> attachment;
 }
