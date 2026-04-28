95e603a7af7a462f653002921d89f8033fc19d2c
deji <ayodeji2k@gmail.com>
2026-04-28 03:03:16 +0100

    add sendAll method to MailGateway for bulk email sending with concurrency support

--- src/main/java/xy/mailsenders/service/MailGateway.java
+++ src/main/java/xy/mailsenders/service/MailGateway.java
@@ -1,8 +1,14 @@
 package xy.mailsenders.service;
 
+import xy.mailsenders.mail.domain.MailFailure;
 import xy.mailsenders.mail.domain.MailPayload;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface MailGateway {
 
     void send(MailPayload payload);
+    List<MailFailure> sendAll(Collection<MailPayload> payloads, int concurrency);
+
 }
