034bdbd1fa73acedd3ba26b9c2bebe0c2da2e8cd
deji <ayodeji2k@gmail.com>
2026-05-28 02:31:23 +0100

    add BrevoSendEmailRequest class for constructing email send requests

---
+++ src/main/java/xy/mailsenders/mail/brevo/request/BrevoSendEmailRequest.java
@@ -0,0 +1,18 @@
+package xy.mailsenders.mail.brevo.request;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class BrevoSendEmailRequest {
+
+    private BrevoEmailContact sender;
+    private List<BrevoEmailContact> to;
+    private String subject;
+    private String htmlContent;
+    private String textContent;
+    private Map<String, String> headers;
+    private List<BrevoAttachment> attachment;
+}
